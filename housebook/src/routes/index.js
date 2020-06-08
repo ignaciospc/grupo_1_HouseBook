6e9666de53067b421590e4eb25bdaa4127f2f0f7
ignaciospc <ignacio.n.soria@hotmail.com>
2020-06-08 17:40:58 -0300

    funciona el subir img

--- housebook/src/routes/index.js
+++ housebook/src/routes/index.js
@@ -1,19 +1,37 @@
 const express = require('express');
 const router = express.Router();
-const controllerIndex = require("../controllers/index")
+const controllerIndex = require("../controllers/index");
+const multer = require("multer");
+const path = require("path");
+
+//*******************variable para Subir Imagenes*******************/
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '../public/images/libros')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+
+      console.log(file);
+      
+    }
+    
+  })
+   
+  var upload = multer({ storage: storage })
 
 
 /* GET home page. */
 router.get('/', controllerIndex.home);
 
 //rotuer.get Productos
-router.get("/products", controllerIndex.products) 
+router.get("/products", controllerIndex.products);
 
-router.get("/products/create",controllerIndex.create)
-router.post("/products",controllerIndex.createBook)
-router.get("/products/:id", controllerIndex.details)
+router.get("/products/create",controllerIndex.create);
+router.post("/products", upload.any(), controllerIndex.createBook);
+router.get("/products/:id", controllerIndex.details);
 
-router.get("/cart", controllerIndex.cart)
+router.get("/cart", controllerIndex.cart);
 
 
 module.exports = router;
