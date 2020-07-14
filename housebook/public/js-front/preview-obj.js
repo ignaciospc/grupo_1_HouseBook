6ee6af90160a7a8405f44d785b7844c6b9065e1e
ignaciospc <ignacio.n.soria@hotmail.com>
2020-07-14 00:08:32 -0300

    se agrego la vista a la preview del home

--- housebook/public/js-front/preview-obj.js
+++ housebook/public/js-front/preview-obj.js
@@ -52,7 +52,50 @@ window.onload = () => {
             let stick = dad.querySelector("#" + targetId) // https://stackoverflow.com/questions/5783969/how-to-get-child-element-by-id-in-javascript/5784028
             if (!stick) { //de existir true, de no existir false | de existir devolver el objeto, de no existir crearlo y devolverlo.
                 dad.innerHTML += `
-                <div class = popUp id="${targetId}" style ="display: none;">Hola soy un preview del ${element.getAttribute("idElement")}</div>` 
+                <div class = popUp id="${targetId}" style ="display: none;">
+
+                <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>preview</title>
+    <link rel="stylesheet" href="/css/preview-products.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+
+
+<div class="container-preview">
+    <div class="titulo-preview">
+        ACA VA EL TITULO DEL LIBRO BIEN BONITO
+    </div>
+    <div class="img-portada-preview"><img src=/images/libros/libro-prueba.jpg alt="img-libro"></div>
+
+    <div class="info-preview">
+        <div class="datos"> <label for="">Autor :</label><span>Vargallosa</span></div>
+        <div class="datos"> <label for="">valoracion :</label><span><i class="fas fa-star"></i>5</span></div>
+        <div class="datos"> <label for="">Categoria :</label><span>Fiction</span></div>
+        <div class="datos"> <label for="">Precio :</label><span>$1550</span></div>
+        <div class="datos"> <label for="">Idioma :</label><span>Spanish</span></div>
+        <div class="datos"> <label for="">Formato :</label><span>Ambos</span></div>
+        
+        
+        
+        
+        
+    </div>
+    <div class="descripcion-preview">
+        <p class="descripcion-libro"> <span>Descripcion : </span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio voluptate ea aspernatur possimus natus nihil labore sed, dolorum facilis aut unde. Commodi esse dolores eligendi laborum! Laudantium architecto necessitatibus praesentium blanditiis. Fuga fugit nisi et nobis quas vel, quod illum</p>
+    </div>
+
+</div>
+
+</body>
+</html>
+                
+                </div>` 
                 stick = dad.querySelector("#" + targetId) //lo actualizo para que exista.
                 
                 let stickX =  getComputedStyle(stick).width,
